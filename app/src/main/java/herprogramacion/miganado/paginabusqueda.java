ac81e906edbe75de236d959bb945970843bb1846
SergioMota92 <100282524@alumnos.uc3m.es>
2016-11-24 11:59:30 +0100

    Prueba

--- app/src/main/java/herprogramacion/miganado/paginabusqueda.java
+++ app/src/main/java/herprogramacion/miganado/paginabusqueda.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class paginabusqueda extends AppCompatActivity {
     @Override
+
+
+    /**/
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paginabusqueda);
