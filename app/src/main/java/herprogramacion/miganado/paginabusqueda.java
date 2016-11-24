f8214f66867a4df62ba9fd8b23ee695fb10f68b2
igarzon <isidoro.garzonherrero@room714.com>
2016-11-24 11:38:39 +0100

    Initial commit

---
+++ app/src/main/java/herprogramacion/miganado/paginabusqueda.java
@@ -0,0 +1,12 @@
+package herprogramacion.miganado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class paginabusqueda extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paginabusqueda);
+    }
+}
