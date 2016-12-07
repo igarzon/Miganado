74843362b3192ec9f1633878711cd840785a167b
igarzon <isidoro.garzonherrero@room714.com>
2016-12-07 16:02:40 +0100

    Registro y Login conectados a BBDD. Se pueden  crear usuarios y probarlos con el login

---
+++ app/src/main/java/miganado/Loginyregistro/RegisterRequest.java
@@ -0,0 +1,36 @@
+package miganado.Loginyregistro;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by User on 07/12/2016.
+ */
+
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "http://Sample-env-1.jqgicx3rm6.eu-west-1.elasticbeanstalk.com/movil/registro.php";
+    private Map<String, String> params;
+
+    public RegisterRequest (String username, String password, String password2 ,String nombre, String tipoacceso, String dato1, String dato2, String dato3, String dato4, String dato5, String dato6, Response.Listener<String> Listener){
+        super (Request.Method.POST, REGISTER_REQUEST_URL, Listener, null);
+        params = new HashMap<>();
+        params.put ("username", username);
+        params.put ("password", password);
+        params.put ("password2", password2);
+        params.put ("nombre", nombre);
+        params.put ("tipoacceso", tipoacceso);
+        params.put ("dato1", dato1);
+        params.put ("dato2", dato2);
+        params.put ("dato3", dato3);
+        params.put ("dato4", dato4);
+        params.put ("dato5", dato5);
+        params.put ("dato6", dato6);
+    }
+    @Override
+    public Map<String,String> getParams (){
+        return params;}
+}
--- app/src/main/java/miganado/loginyregistro/RegistroActivity.java
+++ app/src/main/java/miganado/loginyregistro/RegistroActivity.java
@@ -1,7 +1,21 @@
 package miganado.Loginyregistro;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -9,5 +23,64 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etPassword2 = (EditText) findViewById(R.id.etPassword2);
+        final EditText etNombre = (EditText) findViewById(R.id.etNombreyapellidos);
+        final RadioGroup rbTipoacceso = (RadioGroup) findViewById(R.id.rbTipoacceso);
+        final Button btnRegisro = (Button) findViewById(R.id.btnRegistro);
+        final EditText etDato1 = (EditText) findViewById(R.id.etDato1);
+        final EditText etDato2 = (EditText) findViewById(R.id.etDato2);
+        final EditText etDato3 = (EditText) findViewById(R.id.etDato3);
+        final EditText etDato4 = (EditText) findViewById(R.id.etDato4);
+        final EditText etDato5 = (EditText) findViewById(R.id.etDato5);
+        final EditText etDato6 = (EditText) findViewById(R.id.etDato6);
+
+        btnRegisro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                final String password2 = etPassword2.getText().toString();
+                final String nombre = etNombre.getText().toString();
+                final String tipoacceso = etNombre.getText().toString();
+                final String dato1 = etDato1.getText().toString();
+                final String dato2 = etDato2.getText().toString();
+                final String dato3 = etDato3.getText().toString();
+                final String dato4 = etDato4.getText().toString();
+                final String dato5 = etDato5.getText().toString();
+                final String dato6 = etDato6.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            String error=jsonResponse.getString("message");
+                            if (success){
+                                Intent intent=new Intent(RegistroActivity.this, LoginActivity.class);
+                                intent.putExtra("username", username);
+                                RegistroActivity.this.startActivity(intent);
+                            } else {
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder (RegistroActivity.this);
+                                builder.setMessage( "Ha fallado el registro: " + error )
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }
+
+
+                        } catch (JSONException e){e.printStackTrace();}
+                    }
+                };
+                RegisterRequest RegisterRequest = new RegisterRequest(username, password, password2, nombre, tipoacceso, dato1, dato2, dato3, dato4, dato5, dato6, responseListener);
+                RequestQueue queue= Volley.newRequestQueue(RegistroActivity.this);
+                queue.add(RegisterRequest);
+            }
+        });
+
+
     }
 }
